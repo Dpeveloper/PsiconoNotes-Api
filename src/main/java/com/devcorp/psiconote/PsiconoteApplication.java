d770ed79387a1cb4358c1f559ce7b8ef66540166
Dpeveloper <dqp235497@gmail.com>
2024-05-22 09:46:13 -0500

    Inicializacion del proyecto

---
+++ src/main/java/com/devcorp/psiconote/PsiconoteApplication.java
@@ -0,0 +1,13 @@
+package com.devcorp.psiconote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsiconoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsiconoteApplication.class, args);
+	}
+
+}
