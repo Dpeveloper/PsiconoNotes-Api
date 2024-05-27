1836e55cb29357802eeeaf13226a52b26828cc8d
Pablo Barrera <pabloandresbarrera0@gmail.com>
2024-05-27 01:05:52 -0500

    Corrección entidad problema

--- src/main/java/com/devcorp/psiconote/entities/SesionCancelada.java
+++ src/main/java/com/devcorp/psiconote/entities/SesionCancelada.java
@@ -1,10 +1,41 @@
 package com.devcorp.psiconote.entities;
 
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 @Table(name = "sesiones_canceladas")
-public class SesionCancelada extends Sesion{
+public class SesionCancelada{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime fechaYHora;
+    private String lugarSesion;
+
+    //paciente dentro de la sesión
+    @OneToOne(targetEntity = Paciente.class,fetch = FetchType.EAGER)
+    @JoinColumn(name = "idPaciente",referencedColumnName = "id")
+    private Paciente paciente;
+
+    //psicologo dentro de la sesión
+    @OneToOne(targetEntity = Psicologo.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "idPsicologo",referencedColumnName = "id")
+    private Psicologo psicologo;
+
+    //estado de la sesión
+    @ManyToOne(targetEntity = Estado.class,fetch = FetchType.EAGER)
+    @JoinColumn(name="idEstado",referencedColumnName = "id")
+    private Estado estado;
+
     private String motivoCancelacion;
 }
